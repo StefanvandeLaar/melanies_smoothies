686c4e7ab23b2b05ae73bd1b9e420d2c7216966c
StefanvandeLaar <stefanvdlaar@gmail.com>
2026-04-14 07:46:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     #st.write(ingredients_string)
 
@@ -48,10 +50,6 @@ if ingredients_list:
     #st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
-
-import requests
-smoothiefroot_response =requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
